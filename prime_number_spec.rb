7de2c4ccda8447eb8e3cc263be46ce8e7dc49bf8
Tomas Valent <equivalent@eq8.eu>
2012-05-13 18:44:16 +0100

    failing spec for generating prime numbers

--- prime_number_spec.rb
+++ prime_number_spec.rb
@@ -5,10 +5,13 @@ describe PrimeNumber do
   describe 'Class method' do
     describe 'generate()' do
       it 'should return so many numbers as argument specify' do
+        PrimeNumber.generate(10).count.should == 10
       end
       it 'shuld return only prime numbers' do
+       PrimeNumber.generate(11).should == ('2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31'.split(', '))
       end
       it 'returned numbres should be uniq' do
+        PrimeNumber.generate(10)
       end
     end
   end
