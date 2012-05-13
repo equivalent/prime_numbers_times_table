ef205b42e68f49a3973d03148da5c9ad42bd746a
Tomas Valent <equivalent@eq8.eu>
2012-05-13 18:44:00 +0100

    failing spec for generating prime numbers

---
+++ prime_number_spec.rb
@@ -0,0 +1,24 @@
+#PrimeNumber spec
+require './prime_number'
+describe PrimeNumber do
+  
+  describe 'Class method' do
+    describe 'generate()' do
+      it 'should return so many numbers as argument specify' do
+      end
+      it 'shuld return only prime numbers' do
+      end
+      it 'returned numbres should be uniq' do
+      end
+    end
+  end
+
+  describe 'initialize' do
+    xit 'should set first 10 numbers by default'
+  end
+
+  describe 'generate_times_table' do
+
+  end
+
+end
